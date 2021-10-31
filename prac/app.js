c423ed300c164f2749e6f689b6611f9a5514d90f
apicad0307 <32051350+apicad0307@users.noreply.github.com>
2021-10-31 21:09:09 +0900

    prac1
    class

---
+++ prac/app.js
@@ -0,0 +1,11 @@
+class person {
+  constructor(name, age){
+    this.name = name;
+    this.age = age;
+  }
+
+}
+
+const hoin = new person('hoin', 25);
+console.log(hoin.name);
+console.log(hoin.age);
