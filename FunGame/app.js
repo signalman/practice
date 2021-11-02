9483fb1d1da9949f1d9273414651722761c15da0
apicad0307 <32051350+apicad0307@users.noreply.github.com>
2021-11-02 22:22:12 +0900

    js: fungame sol
    fungame solution

--- FunGame/app.js
+++ FunGame/app.js
@@ -10,12 +10,17 @@ function gameStart(){
     bg.pause();
     i.classList.remove('fa-stop');
     i.classList.add('fa-play');
+    //STOP MESSAGE
+    //yes
+
+    //no
   }
   else{
-    countStart();
-    // countNumberOfCarrots();
-    createCarrot();
     bg.play();
+    countStart();
+    createCarrot();
+    createBugs();
+    // countNumberOfCarrots();
     i.classList.remove('fa-play');
     i.classList.add('fa-stop');
 
@@ -38,5 +43,32 @@ function createCarrot(){
   img.style.position='absolute';
   img.style.top=top+'px';
   img.style.left=left+'px';
-  }
+  img.addEventListener('mouseover', (event)=>{
+    event.target.style.cursor ='pointer'
+  })
+  img.addEventListener('click', (event)=>{
+    event.target.remove();
+  })
+}
+}
+function createBugs(){
+  for(let i=0; i<10; i++){
+    const i = 0;
+    let top = Math.random();
+    top=top*190;
+    let left = Math.random();
+    left=left*700;
+    const img = document.createElement('img');
+    img.src= "img/bug.png";
+    document.body.querySelector('.box2').appendChild(img);
+    img.style.position='absolute';
+    img.style.top=top+'px';
+    img.style.left=left+'px';
+    img.addEventListener('mouseover', (event)=>{
+      event.target.style.cursor ='pointer'
+    })
+    img.addEventListener('click', ()=>{
+      alert('Game Over!!')
+    })
+    }
 }
