92b0deda067aeb3f8f0d228c41ab061f71e7b0ba
apicad0307 <32051350+apicad0307@users.noreply.github.com>
2021-11-03 02:31:09 +0900

    FunGame Refactoring
    FunGame Refactoring

---
+++ FunGame copy/src/popup.js
@@ -0,0 +1,24 @@
+export default class PopUP {
+  constructor() {
+    this.popUp = document.querySelector(".pop-up");
+    this.popUpText = document.querySelector(".pop-up__message");
+    this.popUpRefresh = document.querySelector(".pop-up__refresh");
+    this.popUpRefresh.addEventListener('click',()=>{
+      this.onClick && this.onClick();
+      this.hide();
+    });
+  }
+
+  setClickListner(onClick){
+    this.onClick = onClick;
+  }
+
+  showWithText(text){
+    this.popUpText.innerHTML = text;
+    this.popUp.classList.remove('pop-up--hide');
+  }
+
+  hide(){
+    this.popUp.classList.add('pop-up--hide');
+  }
+}
